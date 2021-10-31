80ce3c4cb72274f1e09b17a66485586ec52c7242
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-10-31 20:24:42 +0800

    Defined Operator01 - Used MthModule

--- EstimNumApl.py
+++ EstimNumApl.py
@@ -1,3 +1,5 @@
+import math
+
 def user_money():
     usr_balance = float(input("Enter your balance: "))
     return usr_balance
@@ -6,7 +8,14 @@ def apl_price():
     cost = float(input("\nEnter the price of an apple: "))
     return cost
 
+def quantity_operator(balance, price):
+    max_no_apl = math.floor(balance//price) 
+    return max_no_apl
+
 dividend = user_money()
 divisor = apl_price()
-print(f"\nBalance: {dividend} \nPrice of an Apple: {divisor}")
+
+output_01 = quantity_operator(dividend, divisor)
+
+print(f"\nYou can purchase {output_01} apples.")
 
