d88cfb97b1dece2831936bf59b2414ef5d0818d1
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-10-31 20:30:18 +0800

    Defined Operator02 - Code Completed

--- EstimNumApl.py
+++ EstimNumApl.py
@@ -11,11 +11,15 @@ def apl_price():
 def quantity_operator(balance, price):
     max_no_apl = math.floor(balance//price) 
     return max_no_apl
+def change_operator(balance, price):
+    change = balance % price
+    return change
 
 dividend = user_money()
 divisor = apl_price()
 
 output_01 = quantity_operator(dividend, divisor)
+output_02 = change_operator(dividend, divisor)
 
-print(f"\nYou can purchase {output_01} apples.")
+print(f"\nYou can purchase {output_01} apples and your change is {output_02:.2f} pesos.")
 
