d8401e17249ec6262da9dbcfdfa90296c17ca6a0
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2021-10-31 19:38:33 +0800

    Defined Operations - Code Complete

--- TPriceComp_Def.py
+++ TPriceComp_Def.py
@@ -6,10 +6,17 @@ def orng_input():
     QuantOrng = int(input("\nHow many oranges?\n> "))
     return QuantOrng
 
+def total_prc(Apl_quant, Orng_quant):
+    product_01 = Apl_quant*20
+    product_02 = Orng_quant*25
+    summation = product_01 + product_02
+    return summation
+
 Apple = apl_input()
 Orange = orng_input()
 
-
+grnd_total = total_prc(Apple, Orange)
+print(f"\nThe total amount is {grnd_total}.")
 
 
 
